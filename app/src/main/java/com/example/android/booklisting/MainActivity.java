4bcdda0df050ec14870a591197a5953438e33759
Seth Watkins <seth.personal.smtp@gmail.com>
2017-07-18 11:55:44 -0500

    Implemented the LoaderManager.LoaderCallbacks interface on MainActivity.

--- app/src/main/java/com/example/android/booklisting/MainActivity.java
+++ app/src/main/java/com/example/android/booklisting/MainActivity.java
@@ -1,16 +1,34 @@
 package com.example.android.booklisting;
 
+import android.app.LoaderManager;
+import android.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import java.net.HttpURLConnection;
+import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<Book>> {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    }
+
+    @Override
+    public Loader<List<Book>> onCreateLoader(int id, Bundle args) {
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<List<Book>> loader, List<Book> data) {
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<List<Book>> loader) {
+        
     }
 }
