617f06da9745d331732c06cc4db3360bfa075d1b
Seth Watkins <seth.personal.smtp@gmail.com>
2017-07-19 06:06:39 -0500

    onCreateLoader was returning null instead of the intended BookLoader object

--- app/src/main/java/com/example/android/booklisting/BookLoader.java
+++ app/src/main/java/com/example/android/booklisting/BookLoader.java
@@ -2,6 +2,7 @@ package com.example.android.booklisting;
 
 import android.content.AsyncTaskLoader;
 import android.content.Context;
+import android.util.Log;
 
 import java.net.HttpURLConnection;
 import java.util.ArrayList;
@@ -28,8 +29,9 @@ public class BookLoader extends AsyncTaskLoader<List<Book>> {
 
         //ArrayList to be returned to begin testing networking code
         List<Book> bookList = new ArrayList<Book>();
-        bookList.add(1, new Book("Test Title", (new String[]{"Actor 1", "Actor 2"}), "Fake Description", 12, 2.20));
+        bookList.add(0, new Book("Test Title", (new String[]{"Actor 1", "Actor 2"}), "Fake Description", 12, 2.20));
 
+        Log.v("BookLoader" , "I am about to make the connection and run the query in loadInBackground");
         HttpURLConnection testHttpURL = QueryHandler.makeConnection(QueryHandler.createURL(QueryHandler.TEST_URL));
 
         return bookList;
