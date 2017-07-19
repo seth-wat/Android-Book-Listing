f05d03a8b60f8ef007673b27465398c03018702e
Seth Watkins <seth.personal.smtp@gmail.com>
2017-07-19 08:46:54 -0500

    Clean up test code and return real data.

--- app/src/main/java/com/example/android/booklisting/BookLoader.java
+++ app/src/main/java/com/example/android/booklisting/BookLoader.java
@@ -26,14 +26,11 @@ public class BookLoader extends AsyncTaskLoader<List<Book>> {
 
     @Override
     public List<Book> loadInBackground() {
-
-        //ArrayList to be returned to begin testing networking code
-        List<Book> bookList = new ArrayList<Book>();
-        bookList.add(0, new Book("Test Title", (new String[]{"Actor 1", "Actor 2"}), "Fake Description", 12, 2.20));
+        List<Book> bookList = null;
 
         Log.v("BookLoader" , "I am about to make the connection and run the query in loadInBackground");
-        HttpURLConnection testHttpURL = QueryHandler.makeConnection(QueryHandler.createURL(QueryHandler.TEST_URL));
-        QueryHandler.parseJSONData(QueryHandler.getRawJSONFromStream(testHttpURL));
+        bookList = QueryHandler.fetchBookData(QueryHandler.TEST_URL);
+        Log.v("BookLoader", "I've finshed fetching the book data");
         return bookList;
     }
 }
