71492fb92a616930cfaa6e568841274fd2a77583
Seth Watkins <seth.personal.smtp@gmail.com>
2017-07-20 13:31:25 -0500

    Revise Loader init, handle query submission from menu.

--- app/src/main/java/com/example/android/booklisting/MainActivity.java
+++ app/src/main/java/com/example/android/booklisting/MainActivity.java
@@ -6,6 +6,11 @@ import android.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.SearchView;
 import android.widget.TextView;
@@ -16,25 +21,41 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<Book>> {
     ListView mListView;
+    BookArrayAdapater mAdapter;
+    EditText mEditView;
+    //this will server as the unique loader id.
+    int queryClicks;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mListView = (ListView) findViewById(R.id.display_list_view);
-        getLoaderManager().initLoader(1, null, this);
-        SearchView sv = (SearchView) findViewById(R.id.book_search_view);
-        sv.setSubmitButtonEnabled(true);
-        sv.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+        mAdapter = new BookArrayAdapater(this, new ArrayList<Book>());
+        mListView.setAdapter(mAdapter);
+        mEditView = (EditText) findViewById(R.id.book_edit_text);
+        mEditView.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onQueryTextChange(String newText) {
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                return false;
+            public void onClick(View v) {
+                mEditView.setText("");
             }
         });
+        
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.search_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.menu_search_item) {
+            QueryHandler.setUrl(QueryHandler.BASE_URL + mEditView.getText().toString() + "&maxResults=10");
+            getLoaderManager().initLoader(queryClicks, null, MainActivity.this);
+        }
+        return true;
     }
 
     @Override
@@ -46,8 +67,8 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     @Override
     public void onLoadFinished(Loader<List<Book>> loader, List<Book> data) {
         Log.v("MainActivity", "onLoadFinished is now executing");
-        BookArrayAdapater mAdapter = new BookArrayAdapater(this, (ArrayList<Book>) data);
-        mListView.setAdapter(mAdapter);
+        mAdapter.clear();
+        mAdapter.addAll(data);
     }
 
     @Override
